d57deac9be9fef5cbb3aa396b8b33bcdf40062cf
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-11-04 22:48:45 +0530

    :sparkles:Added player fun

--- tictactoe.py
+++ tictactoe.py
@@ -19,3 +19,21 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY]]
 
 
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if terminal(board):
+        return EMPTY
+    x = 0
+    o = 0
+    for list in board:
+        for cell in list:
+            if cell == X:
+                x += 1
+            elif cell == O:
+                o += 1
+    if x == o:
+        return X
+    return O
+    
