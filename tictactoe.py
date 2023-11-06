35c74a8ffdffb4ca9e430a9b1256c4e1a3c2e714
Aakash Kumar <31201591+Kumar-Aakash@users.noreply.github.com>
2023-11-06 22:08:27 +0530

    :necktie: Added Actions and Result feature

--- tictactoe.py
+++ tictactoe.py
@@ -10,6 +10,7 @@ X = "X"
 O = "O"
 EMPTY = None
 
+
 def initial_state():
     """
     Returns starting state of the board.
@@ -37,3 +38,35 @@ def player(board):
         return X
     return O
     
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    s = set()
+    if terminal(board):
+        return s 
+
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == EMPTY:
+                s.add((i,j))
+
+    return s 
+    
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    if action[0] < 0 or action[0] > 2 or action[1] < 0 or action[1] > 2:
+        raise ValueError("Invalid Cordination")
+    elif board[action[0]][action[1]] == X or board[action[0]][action[1]] == O or board[action[0]][action[1]] != EMPTY:
+        raise ValueError("Cell is already occupaid.")
+    
+    move = player(board)
+    res_board = copy.deepcopy(board)
+    res_board[action[0]][action[1]] = move
+    return res_board
+
+
