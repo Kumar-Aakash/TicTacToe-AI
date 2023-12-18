eadf97091308fb0304151162fc7ccab7bc004fe5
Aakash Kumar <31201591+Kumar-Aakash@users.noreply.github.com>
2023-12-18 22:33:37 +0530

    :construction: working on structure

--- runner.py
+++ runner.py
@@ -16,3 +16,39 @@ screen = pygame.display.set_mode(size)
 mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
 largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
 moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
+
+user = None
+board = ttt.initial_state()
+ai_turn = False
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(black)
+
+    # Let user choose a player.
+    if user is None:
+
+        # Draw title
+        title = largeFont.render("Play Tic-Tac-Toe", True, white)
+        titleRect = title.get_rect()
+        titleRect.center = ((width / 2), 50)
+        screen.blit(title, titleRect)
+
+        # Draw buttons
+        playXButton = pygame.Rect((width / 8), (height / 2), width / 4, 50)
+        playX = mediumFont.render("Play as X", True, black)
+        playXRect = playX.get_rect()
+        playXRect.center = playXButton.center
+        pygame.draw.rect(screen, white, playXButton)
+        screen.blit(playX, playXRect)
+
+        playOButton = pygame.Rect(5 * (width / 8), (height / 2), width / 4, 50)
+        playO = mediumFont.render("Play as O", True, black)
+        playORect = playO.get_rect()
+        playORect.center = playOButton.center
+        pygame.draw.rect(screen, white, playOButton)
+        screen.blit(playO, playORect)
